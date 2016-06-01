ddd6052b4748756aac0243a023a6b96737525665
Patrick Tak <Patrick.Tak@Egm.local>
2016-06-01 12:53:13 +0200

    Initial Release

---
+++ Android_Cardboard/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagement : MonoBehaviour {
+
+    public static GameManagement instance = null;
+
+    public GameObject[] SpawnLocations;
+    public int Score;
+
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        Score = 0;
+
+        SpawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoints");
+	}
+	
+
+    public void updateScore(int ScoreToAdd)
+    {
+        Score += ScoreToAdd;
+    }
+
+}
---
+++ Android_Cardboard/Assets/Scripts/TargetLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetLogic : MonoBehaviour {
+
+    private int TargetValue;
+    private GameManagement gm;
+    private AudioSource quack;
+
+    // Use this for initialization
+	void Start () {
+        TargetValue = Mathf.RoundToInt(Random.Range(1.0f, 3.0f));
+        gm = GameManagement.instance;
+        quack = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HitRecieved()
+    {
+        quack.Play();
+        transform.position = gm.SpawnLocations[Mathf.RoundToInt(Random.Range(0, gm.SpawnLocations.Length -1))].transform.position; 
+        gm.updateScore(TargetValue);
+    }
+
+    
+}
